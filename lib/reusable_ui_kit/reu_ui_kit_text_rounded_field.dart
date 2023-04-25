4ab540840f4be8caffe5e13becaf2a4245b5dd0f
unknown <jeremimanogi@gmail.com>
2023-04-25 18:39:43 +0700

    create rounded textfield

---
+++ lib/reusable_ui_kit/reu_ui_kit_text_rounded_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:form_tutorial/reusable_ui_kit/reu_ui_kit_text_field.dart';
+
+class ReuUiKitTextRoundedField extends StatefulWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final int maxLines;
+  final InputBorder? border;
+  const ReuUiKitTextRoundedField({
+    super.key,
+    required this.labelText,
+    required this.controller,
+    this.maxLines = 1,
+    this.border,
+  });
+
+  @override
+  State<ReuUiKitTextRoundedField> createState() =>
+      _ReuUiKitTextRoundedFieldState();
+}
+
+class _ReuUiKitTextRoundedFieldState extends State<ReuUiKitTextRoundedField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        border: Border.all(color: Colors.black),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            6.0,
+          ),
+        ),
+      ),
+      child: ReuUiKitTextField(
+        labelText: widget.labelText,
+        controller: widget.controller,
+        border: widget.border,
+        maxLines: widget.maxLines,
+      ),
+    );
+  }
+}
