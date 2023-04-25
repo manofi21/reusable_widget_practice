c0880d432b01673e5df32e661b01ccb06fa09dc1
unknown <jeremimanogi@gmail.com>
2023-04-25 17:50:10 +0700

    create field phone number

---
+++ lib/module/dashboard/widget/reu_ui_kit_text_field_phone_number.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:form_tutorial/reusable_ui_kit/reu_ui_kit_text_field.dart';
+
+Widget get prefixRegionINDOCall => Container(
+      margin: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+      child: const Text('+62', style: TextStyle(fontSize: 15)),
+    );
+
+class ReuUiKitTextFieldPhoneNumber extends StatelessWidget {
+  final TextEditingController controller;
+  const ReuUiKitTextFieldPhoneNumber({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return ReuUiKitTextField(
+      labelText: 'Phone Number',
+      controller: controller,
+      usePrefix: true,
+      prefixWidget: prefixRegionINDOCall,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return "Phone Number Must be Filled";
+        }
+
+        if (int.tryParse(value ?? '') == null) {
+          return "Phone Number Must Be Numeric";
+        }
+
+        return null;
+      },
+    );
+  }
+}
