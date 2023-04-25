adccc734ca5481fc1670f9a290c22cb6c2eba394
unknown <jeremimanogi@gmail.com>
2023-04-25 18:40:17 +0700

    create textfield address

---
+++ lib/module/dashboard/widget/reu_ui_kit_text_field_address.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:form_tutorial/reusable_ui_kit/reu_ui_kit_text_rounded_field.dart';
+
+class ReuUiKitTextFieldAddress extends StatelessWidget {
+  final TextEditingController controller;
+  const ReuUiKitTextFieldAddress({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return ReuUiKitTextRoundedField(
+      labelText: "Address",
+      controller: controller,
+      maxLines: 3,
+      border: InputBorder.none,
+    );
+  }
+}
