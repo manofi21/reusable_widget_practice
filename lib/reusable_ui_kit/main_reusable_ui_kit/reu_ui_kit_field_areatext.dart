6cfe54b066da9dcb0745f85b7de6de3b5ea73942
unknown <jeremimanogi@gmail.com>
2023-05-04 06:03:25 +0700

    add parameter useShadowBox in area text reusable

--- lib/reusable_ui_kit/main_reusable_ui_kit/reu_ui_kit_field_areatext.dart
+++ lib/reusable_ui_kit/main_reusable_ui_kit/reu_ui_kit_field_areatext.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:form_tutorial/reusable_ui_kit/base_reusable_ui_kit/reu_ui_kit_text_rounded_field.dart';
+import 'package:form_tutorial/reusable_ui_kit/input_decoration_bold_shadow.dart';
+
+import '../base_reusable_ui_kit/reu_ui_kit_text_field.dart';
 
 class ReuUiKitTextFieldAreaText extends StatelessWidget {
   final TextEditingController controller;
   final String labelText;
   final int maxLines;
+  final bool useShadowBox;
+  final InputBorder? border;
+
   const ReuUiKitTextFieldAreaText({
     super.key,
     required this.controller,
     required this.labelText,
     this.maxLines = 3,
+    this.useShadowBox = false,
+    this.border,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ReuUiKitTextRoundedField(
+    final keyboardType =
+        maxLines > 1 ? TextInputType.multiline : TextInputType.text;
+    if (useShadowBox) {
+      return InputDecorationBoldShabow(
+        labelText: labelText,
+        child: ReuUiKitTextField(
+          controller: controller,
+          maxLines: maxLines,
+          textInputType: keyboardType,
+          border: border,
+        ),
+      );
+    }
+
+    return ReuUiKitTextField(
       labelText: labelText,
       controller: controller,
       maxLines: maxLines,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide.none,
-      ),
+      textInputType: keyboardType,
     );
   }
 }
