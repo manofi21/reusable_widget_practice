1df749ec5aa4f7af0065a142fbb02255ddb3f633
unknown <jeremimanogi@gmail.com>
2023-05-04 06:55:45 +0700

    remove dublicated field label name in username field

--- lib/module/dashboard/widget/reu_ui_kit_text_field_username.dart
+++ lib/module/dashboard/widget/reu_ui_kit_text_field_username.dart
@@ -10,32 +10,16 @@ enum FieldNameType {
 class ReuUiKitTextFieldName extends StatelessWidget {
   final TextEditingController controller;
   final FieldNameType fieldNameType;
-  final bool useShadowBox;
   const ReuUiKitTextFieldName({
     super.key,
     required this.controller,
     required this.fieldNameType,
-    this.useShadowBox = false,
   });
 
   @override
   Widget build(BuildContext context) {
     final isFirstName = fieldNameType == FieldNameType.firstName;
-    if (useShadowBox) {
-      return InputDecorationBoldShabow(
-          labelText: isFirstName ? "First Name" : "Last Name",
-          child: ReuUiKitTextField(
-            controller: controller,
-            validator: (value) {
-              if ((value?.isEmpty ?? true) && isFirstName) {
-                return 'First name is cannot be empty';
-              }
-              return null;
-            },
-          ));
-    }
-    return ReuUiKitTextField(
-      labelText: isFirstName ? "First Name" : "Last Name",
+    final fieldName = ReuUiKitTextField(
       controller: controller,
       validator: (value) {
         if ((value?.isEmpty ?? true) && isFirstName) {
@@ -43,6 +27,10 @@ class ReuUiKitTextFieldName extends StatelessWidget {
         }
         return null;
       },
+    );
+    return InputDecorationBoldShabow(
+      labelText: isFirstName ? "First Name" : "Last Name",
+      child: fieldName,
     );
   }
 }
