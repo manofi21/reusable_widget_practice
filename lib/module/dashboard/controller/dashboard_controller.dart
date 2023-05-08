753268f847c6401166451dd2e3e6951f14eeaf76
unknown <jeremimanogi@gmail.com>
2023-05-08 20:37:09 +0700

    impl loading dialog in dashboard controller

--- lib/module/dashboard/controller/dashboard_controller.dart
+++ lib/module/dashboard/controller/dashboard_controller.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:form_tutorial/state_util.dart';
 import '../../../function/get_image_to_list.dart';
 import '../view/dashboard_view.dart';
+import 'function/show_dialog_function_for_dashboard.dart';
 
 class DashboardController extends State<DashboardView>
     implements MvcController {
@@ -45,6 +48,31 @@ class DashboardController extends State<DashboardView>
   void clearListImg() {
     listImg.clear();
     imagePickerTextController.clear();
+    setState(() {});
+  }
+
+  void onRemoveDataFunction() {
+    removeDataDialog(
+      context,
+      nSaveFuncion: () {
+        _isAlreadySaved = false;
+        setState(() {});
+      },
+    );
+  }
+
+  void onClearImageDialog() {
+    clearImageDialog(
+      context,
+      nSaveFuncion: () {
+        clearListImg();
+      },
+    );
+  }
+
+  Future<void> onSuccessSaveDialog() async {
+    await successSaveDialog(context);
+    _isAlreadySaved = false;
     setState(() {});
   }
 
