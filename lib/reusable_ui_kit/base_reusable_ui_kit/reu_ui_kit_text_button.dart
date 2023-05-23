43fe023c95508f1e6b89c9cce7945a9756c97a29
unknown <jeremimanogi@gmail.com>
2023-05-23 23:48:56 +0700

    impl isUseUIFugi1

--- lib/reusable_ui_kit/base_reusable_ui_kit/reu_ui_kit_text_button.dart
+++ lib/reusable_ui_kit/base_reusable_ui_kit/reu_ui_kit_text_button.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '../container_elevated_solid_color.dart';
 import '../container_solid_shadow_box.dart';
+
+const isUseUIFugi1 = true;
 
 class ReuUiKitButton extends StatelessWidget {
   final String textLabel;
@@ -23,6 +27,32 @@ class ReuUiKitButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (isUseUIFugi1) {
+      return ContainerElevatedSolidColor(
+        color: color,
+        child: InkWell(
+          onTap: onPressed,
+          child: SizedBox(
+            height: height,
+            width: width,
+            child: Center(
+              child: Text(
+                textLabel,
+                style: GoogleFonts.raleway(
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                    fontSize: fontSize,
+                    color: textColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
     return ContainerSolidShadowBox(
       color: color,
       child: InkWell(
